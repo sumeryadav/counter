26ab449cc33ede5bfbe1c341898274ced30f3b89
sumer <sumeryadav14@gmail.com>
2026-04-22 12:23:16 +0530

    Update UI with dynamic background and better styling

--- my-counter/vite.config.js
+++ my-counter/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/my-dynamic-counter/", // Replace with your repo name
 })
